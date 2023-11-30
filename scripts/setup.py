484866784e611e666923632eec10092b7e6b2d86
Aren Marsden <aren.marsden6@gmail.com>
2023-11-30 21:16:27 +0000

    Update script for organisation
    Signed-off-by: Aren Marsden <aren.marsden6@gmail.com>

--- scripts/setup.py
+++ scripts/setup.py
@@ -3,7 +3,11 @@ from pathlib import Path
 
 def __main__():
     for i in range(1, 26):
-        Path(f"../days/day{i}").mkdir(parents=True, exist_ok=True)
-        Path(f"../days/day{i}/.gitkeep").touch()
+        if i < 10:
+            Path(f"../days/day0{i}").mkdir(parents=True, exist_ok=True)
+            Path(f"../days/day0{i}/.gitkeep").touch()
+        else:
+            Path(f"../days/day{i}").mkdir(parents=True, exist_ok=True)
+            Path(f"../days/day{i}/.gitkeep").touch()
 
 __main__()
